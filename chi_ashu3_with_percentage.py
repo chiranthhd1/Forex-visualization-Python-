08d8e5b99e74c88a152677a72e27113c592a40c4
chiranthhd1 <chd@stevens.edu>
2016-03-31 00:11:50 -0400

    changed functionality to work with the start and end date of any year

--- chi_ashu3_with_percentage.py
+++ chi_ashu3_with_percentage.py
@@ -19,8 +19,8 @@ for keys in file_dict.keys():
 		pass
 
 print("-----------Enter Dates-----------")
-end = raw_input("please enter start date in format m/dd/yyyy (eg:3/11/2016) : ")
-start = raw_input("please enter start date in format m/dd/yyyy (eg:3/11/2016) : ")
+end = raw_input(" Please enter start date in format m/dd/yyyy (eg:3/11/2016) : ")
+start = raw_input(" Please enter end date in format m/dd/yyyy (eg:3/11/2016) : ")
 print("\nOUTPUT:")
 
 myData = csv.reader(o)
@@ -42,8 +42,38 @@ o.close()
 
 #The below code works using pandas library dealing with data frames.
 
+if end1 > start1:
 
-value = file.iloc[start1-1:end1, 0]
+	value = file.iloc[start1-1:end1, :]
+else:
+	value = file.iloc[end1+1:start1+1, :]
+
+maxprice = value.iloc[:, 1].values.max()
+index = value.iloc[:, 1].values.argmax()
+print("\nThe maximum Price is %s on %s" %(maxprice,value.iloc[index]))
+
+x=value.iloc[0,1]
+y=value.iloc[-1,1]
+print("\nThe percentage change from %s to %s is %.2f percent " %(value.iloc[0,0], value.iloc[-1,0],(x-y)/x * 100))
+
+maxprice = value.iloc[:, 2].values.max()
+index = value.iloc[:, 2].values.argmax()
+print("\nThe maximum Price is %s on %s" %(maxprice,value.iloc[index]))
+
+maxprice = value.iloc[:, 3].values.max()
+index = value.iloc[:, 3].values.argmax()
+print("\nThe maximum Price is %s on %s" %(maxprice,value.iloc[index]))
+
+maxprice = value.iloc[:, 4].values.max()
+index = value.iloc[:, 4].values.argmax()
+print("\nThe maximum Price is %s on %s" %(maxprice,value.iloc[index]))
+
+
+'''
+#The below code works using pandas library dealing with data frames.
+
+
+value = file.iloc[start1-1:end1, :]
 
 maxPrice = 0
 maxprice = file.iloc[start1-1:end1, 1].values.max()
@@ -65,5 +95,4 @@ maxprice = file.iloc[start1-1:end1, 4].values.max()
 index = file.iloc[start1-1:end1, 4].values.argmax()
 print("\nThe maximum Low Price is %s on %s" %(maxprice,value.iloc[index]))
 
-
-
+'''
